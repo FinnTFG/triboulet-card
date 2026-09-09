2175542ef4b854602dae58cfbe60bbc78a983648
NoctixRex <123258418+FinnTFG@users.noreply.github.com>
2026-09-09 08:58:41 -0400

    Update jokers.py
    Added first 5 jokers, need to add main.py

--- jokers.py
+++ jokers.py
@@ -1,1 +1,7 @@
+jokerMult = 0
 
+joker = jokerMult += 4
+greedy = jokerMult += playedCards.count(rank(["diamonds"])) * 3
+lusty = jokerMult += playedCards.count(rank(["hearts"])) * 3
+wrathful = jokerMult += playedCards.count(rank(["spades"])) * 3
+glutton = jokerMult += playedCards.count(rank(["clubs"])) * 3
